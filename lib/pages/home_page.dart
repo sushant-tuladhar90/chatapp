0d8f6308869097b628c1dc61f3e1907867f058bd
Sushant <susantuladhar90@gmail.com>
2025-01-02 14:00:23 +0545

    third sommit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:chatapp/services/auth_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import 'chat_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,27 +14,70 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // Instance of the auth
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  //signout 
+  // Sign out
   void signOut() {
-    //get auth service
     final authService = Provider.of<AuthService>(context, listen: false);
-
     authService.signOut();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("HomePage"),
+        title: const Text("HomePage"),
         actions: [
-          //sign out
           IconButton(
             onPressed: signOut,
             icon: const Icon(Icons.logout),
           ),
         ],
       ),
+      body: _buildUserList(),
     );
+  }
+
+  // Build a list of users except for the current logged-in user
+  Widget _buildUserList() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('users').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text('Error');
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text('Loading...');
+        }
+        return ListView(
+          children: snapshot.data!.docs
+              .map<Widget>((doc) => _buildUserListItem(doc))
+              .toList(),
+        );
+      },
+    );
+  }
+
+  // Build individual user list item
+  Widget _buildUserListItem(DocumentSnapshot document) {
+    Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+
+    if (_auth.currentUser!.email != data['email']) {
+      return ListTile(
+        title: Text(data['email']),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ChatPage(
+              receiverUserEmail: data['email'],
+              receiverUserId: data['uid'],
+            )),
+          );
+        },
+      );
+    } else {
+      return const SizedBox.shrink();
+    }
   }
 }
